53d5fef5a610911e6cc1bbc7901f198ce4d8b50c
re-frost <felix.rost@robotic-eyes.com>
2023-04-21 17:45:08 +0200

    add dep cpp

---
+++ dapExamples/nvim-dap.lua.cpp
@@ -0,0 +1,18 @@
+local dap = require('dap')
+
+dap.adapters.cpp = {
+  type = 'executable',
+  command = '/usr/bin/lldb-vscode',
+  name = 'lldb'
+}
+
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'cpp',
+    request = "launch",
+    program = "${workspaceFolder}/build/bin/pb-calib-registration",
+    cwd = '${workspaceFolder}/build/bin/',
+    args = { "../../config/config.json" },
+  },
+}
